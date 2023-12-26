24f9c529ae879d216069586139735e7e4d006487
Beedhan <bhuwaibidhan@gmail.com>
2023-12-27 01:09:13 +0545

    chore: package remove

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "@/utils/api";
-import { HydrationOverlay } from "@builder.io/react-hydration-overlay";
 import { Toaster } from "@/components/ui/sonner"
 
 import "@/styles/globals.css";
